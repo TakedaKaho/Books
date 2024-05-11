5a51f10116f25f4e1698cc2bd64f7ec3f973b266
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-05-11 05:29:31 +0000

    link実装　バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,11 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
     redirect_to book_path(book.id)
+  else render :new
+  end 
   end 
 
   def index
